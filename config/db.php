b221756a0125e21c7ab0a33e8fca767e217be4b0
KimTerrence <quineskimterrence@gmail.com>
2025-08-07 21:17:54 +0800

    myqsli connection

--- config/db.php
+++ config/db.php
@@ -1,3 +1,17 @@
-<?php 
+<?php
+// Database credentials
+$host = 'localhost';
+$dbname = 'attendance_db';
+$user = 'root';
+$pass = '';
 
+// Create connection
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+echo 'Connected successfully';
 ?>
